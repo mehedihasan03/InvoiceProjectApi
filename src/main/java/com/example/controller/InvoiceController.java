e5a5da9e0b8e036ed384a44f1119c124e53119a1
mehedihasan03 <mdmehedi03@gmail.com>
2022-02-08 01:22:18 +0600

    added invoice save and getall api

--- src/main/java/com/example/controller/InvoiceController.java
+++ src/main/java/com/example/controller/InvoiceController.java
@@ -1,5 +1,61 @@
 package com.example.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Invoice;
+import com.example.service.InvoiceService;
+
+@RestController
+@CrossOrigin(origins = "*")
 public class InvoiceController {
 
+	@Autowired
+	private InvoiceService invoiceService;
+	
+	@PostMapping(value = "/invoice/save")
+	public ResponseEntity<?> save(@RequestBody Invoice entity) {
+		Map<String, Object> map = new HashMap<>();
+		try {
+			Invoice invoice = invoiceService.save(entity);
+			map.put("message", "Data save successfully");
+			map.put("Data", invoice);
+			map.put("Status code", 200);
+			return ResponseEntity.ok(map);
+		} catch (Exception e) {
+			e.printStackTrace();
+			map.put("message", "Data saved failed");
+			map.put("Data", null);
+			map.put("Status code", 400);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(map);
+		}
+	}
+	
+	@GetMapping(value = "/invoice/getAll")
+	public ResponseEntity<?> getInvoice() {
+		Map<String, Object> map = new HashMap<>();
+		try {
+			List<Invoice> invoice =(List<Invoice>) invoiceService.findAll();
+			map.put("message", "Data get successfully");
+			map.put("Data", invoice);
+			map.put("Status code", 200);
+			return ResponseEntity.ok(map);
+		} catch (Exception e) {
+			e.printStackTrace();
+			map.put("message", "Data fetch failed");
+			map.put("Data", null);
+			map.put("Status code", 400);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(map);
+		}
+	}
 }
